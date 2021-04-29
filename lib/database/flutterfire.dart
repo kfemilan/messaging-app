ce20c56d332aa7423f9c6418436138e6310270b0
Kristoffer Milan <kristoffer.milan.20@usjr.edu.ph>
2021-04-29 20:02:31 +0800

    Added Fonts, Changed Register and Log In UI

--- lib/database/flutterfire.dart
+++ lib/database/flutterfire.dart
@@ -26,3 +26,13 @@ Future<bool> register(String email, String password) async {
     return false;
   }
 }
+
+Future<bool> signOut() async {
+  try {
+    await FirebaseAuth.instance.signOut();
+    return true;
+  } catch (e) {
+    print(e.toString());
+    return false;
+  }
+}
--- lib/models/Constants.dart
+++ lib/models/Constants.dart
@@ -4,3 +4,40 @@ Color primaryLight = Color(0xFF01A38B);
 Color primaryMedium = Color(0xFF006455);
 Color primaryDark = Color(0xFF003F35);
 Color secondary = Colors.white;
+
+ThemeData appTheme = ThemeData(
+  textTheme: TextTheme(
+    headline6:
+        TextStyle(color: secondary, fontWeight: FontWeight.bold, fontSize: 15),
+    headline1: TextStyle(
+        color: primaryMedium,
+        fontWeight: FontWeight.w900,
+        fontSize: 45,
+        fontFamily: 'Barlow'),
+    bodyText2: TextStyle(color: secondary, fontSize: 15)
+  ),
+  
+  primaryColorLight: primaryLight,
+  primaryColor: primaryMedium,
+  primaryColorDark: primaryDark,
+  accentColor: secondary,
+  scaffoldBackgroundColor: secondary,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      primary: primaryLight,
+      onPrimary: secondary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+    ),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      backgroundColor: secondary,
+      primary: primaryLight,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+    ),
+  ),
+);
