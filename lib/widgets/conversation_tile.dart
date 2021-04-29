cf5e6183a28ccb872bfa5df77cf9a57b76a13f8c
Javin <javin.tan.20@usjr.edu.ph>
2021-04-29 12:58:14 +0800

    Initial Implementation
    -Added Constant .dart
    -Added Conversation and Message classes
    -Initial Home Screen implementation

---
+++ lib/widgets/conversation_tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ConversationTile extends StatelessWidget {
+  const ConversationTile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Conversation Tile"),
+    );
+  }
+}
