9ffd4f253181c2fc6665052bd46d362982916f25
Javin <javin.tan.20@usjr.edu.ph>
2021-05-02 13:29:23 +0800

    Polished Home Page even more
    -image now uses CircleAvatar class
    -tweaked design of the tile messages
    -added dismissables to both left and right sides

--- lib/widgets/conversation_tile.dart
+++ lib/widgets/conversation_tile.dart
@@ -21,57 +21,95 @@ class ConversationTile extends StatelessWidget {
     else
       time = DateFormat.MMMd().format(message.timeSent); // MonthAbbr Date e.g. Mar 1
 
-    return InkWell(
-      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ConversationScreen())),
-      child: Container(
-        height: 75.0,
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        margin: EdgeInsets.symmetric(vertical: 1.0),
-        alignment: Alignment.center,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Profile Picture
-            Container(
-              height: 60.0,
-              width: 60.0,
-              margin: EdgeInsets.only(left: 10.0, right: 15.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Theme.of(context).primaryColorLight),
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                // color: Colors.red,
-              ),
-              child: Image(
-                width: 60,
-                height: 60,
-                image: NetworkImage(kDefaultProfilePicture),
-                color: Theme.of(context).primaryColorLight,
-              ),
-            ),
-
-            // Center Text
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "2Change2Sender:",
-                    style: TextStyle(color: Colors.black),
+    return Dismissible(
+      key: Key(message.toString()),
+      background: Container(
+        alignment: Alignment.centerLeft,
+        margin: EdgeInsets.only(left: 20.0),
+        child: Icon(Icons.more, color: Theme.of(context).primaryColorLight, size: 30),
+      ),
+      secondaryBackground: Container(
+        alignment: Alignment.centerRight,
+        margin: EdgeInsets.only(right: 20.0),
+        child: Icon(Icons.delete, color: Theme.of(context).primaryColorLight, size: 30),
+      ),
+      confirmDismiss: (direction) async {
+        bool dismiss = false;
+        if (direction == DismissDirection.endToStart) {
+          // Delete
+          dismiss = await showDialog<bool>(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                title: Text("Delete Conversation?", style: Theme.of(context).textTheme.bodyText1),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text("No", style: TextStyle(color: Colors.grey)),
+                    onPressed: () => Navigator.of(context).pop(false),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("${this.message.message}", style: TextStyle(color: Colors.black)),
-                      Text("| $time", style: TextStyle(color: Colors.black)),
-                    ],
-                  ), // Timestamp
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Theme.of(context).primaryColorLight),
+                    child: TextButton(
+                      style: TextButton.styleFrom(backgroundColor: primaryLight),
+                      child: Text("Yes", style: TextStyle(color: Colors.white)),
+                      onPressed: () => Navigator.of(context).pop(true),
+                    ),
+                  ),
                 ],
+              );
+            },
+          );
+        } else {
+          // More
+          print("More");
+        }
+        return dismiss;
+      },
+      child: InkWell(
+        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ConversationScreen())),
+        child: Container(
+          height: 75.0,
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          margin: EdgeInsets.symmetric(vertical: 1.0),
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Profile Picture
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 15.0),
+                child: CircleAvatar(
+                  radius: 30.0,
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(kDefaultProfilePicture),
+                ),
               ),
-            ),
-          ],
+              // Center Text
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "2Change2Sender",
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 16.0),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("${this.message.message}", style: TextStyle(color: Colors.grey)),
+                        Text("$time", style: TextStyle(color: Colors.black)),
+                      ],
+                    ), // Timestamp
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
