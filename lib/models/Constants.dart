adf8eb9e7923abe65ac8b408f6a6d4767a1f0c1c
Javin <javin.tan.20@usjr.edu.ph>
2021-05-03 21:48:13 +0800

    ConvoTile Now Sorted, Unneeded Constants Purged
    -title

--- lib/models/Constants.dart
+++ lib/models/Constants.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:messaging_app/models/Conversation.dart';
-import 'package:messaging_app/models/Message.dart';
 
 String kDefaultProfilePicture = "https://www.pngkey.com/png/full/202-2024792_user-profile-icon-png-download-fa-user-circle.png";
 Color primaryLight = Color(0xFF01A38B);
@@ -8,31 +6,13 @@ Color primaryMedium = Color(0xFF006455);
 Color primaryDark = Color(0xFF003F35);
 Color secondary = Colors.white;
 
-Message dummyMessage = Message(message: "Hi, this is a dummy message!", timeSent: DateTime.now());
-List<Conversation> dummyConversations = [
-  Conversation(name: "Bible Studies", latestMessage: Message(message: "Hi", timeSent: DateTime.now())),
-  Conversation(name: "Holiness is Epic", latestMessage: Message(message: "Nice", timeSent: DateTime.now().subtract(Duration(days: 1)))),
-  Conversation(name: "Gimme that 30 bro", latestMessage: Message(message: "Hah?", timeSent: DateTime.now().subtract(Duration(days: 2)))),
-  Conversation(name: "Bible is just a history book", latestMessage: Message(message: "Hatdog", timeSent: DateTime.now().subtract(Duration(days: 3)))),
-  Conversation(name: "Exam Question Leaks", latestMessage: Message(message: "GG", timeSent: DateTime.now().subtract(Duration(days: 4)))),
-  Conversation(name: "Anong Kabostosan Ito?", latestMessage: Message(message: "Talong", timeSent: DateTime.now().subtract(Duration(days: 5)))),
-  Conversation(name: "Barkada GC", latestMessage: Message(message: "Ni", timeSent: DateTime.now().subtract(Duration(days: 6)))),
-  Conversation(
-      name: "Barkada GC w/out that annoying person", latestMessage: Message(message: "Joshua", timeSent: DateTime.now().subtract(Duration(days: 7)))),
-  Conversation(name: "Woke shit", latestMessage: Message(message: "Andre", timeSent: DateTime.now().subtract(Duration(days: 8)))),
-  Conversation(
-      name: "Philosophical Debates that amount to nothing",
-      latestMessage: Message(message: "Nicolai", timeSent: DateTime.now().subtract(Duration(days: 9)))),
-  Conversation(name: "Conversation Name", latestMessage: Message(message: "Dotiloss", timeSent: DateTime.now().subtract(Duration(days: 31)))),
-];
-
 ThemeData appTheme = ThemeData(
   textTheme: TextTheme(
-    headline6: TextStyle(color: secondary, fontWeight: FontWeight.bold, fontSize: 15),
-    headline3: TextStyle(color: primaryMedium, fontWeight: FontWeight.w700, fontSize: 20, fontFamily: 'Barlow'),
-    headline1: TextStyle(color: primaryMedium, fontWeight: FontWeight.w900, fontSize: 45, fontFamily: 'Barlow'),
-    bodyText2: TextStyle(color: secondary, fontSize: 15),
-  ),
+      headline6: TextStyle(color: secondary, fontWeight: FontWeight.bold, fontSize: 15),
+      headline3: TextStyle(color: primaryMedium, fontWeight: FontWeight.w700, fontSize: 20, fontFamily: 'Barlow'),
+      headline1: TextStyle(color: primaryMedium, fontWeight: FontWeight.w900, fontSize: 45, fontFamily: 'Barlow'),
+      bodyText2: TextStyle(color: secondary, fontSize: 15),
+      subtitle2: TextStyle(color: Colors.redAccent)),
   primaryColorLight: primaryLight,
   primaryColor: primaryMedium,
   primaryColorDark: primaryDark,
