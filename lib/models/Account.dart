057647a75f8e54eacc92df787d9d2f797b9acee3
Javin <javin.tan.20@usjr.edu.ph>
2021-05-03 15:11:33 +0800

    Created getFirstNames() function
    -parameters is passing List of Accounts
    -returns the Firsnames of said Accounts

--- lib/models/Account.dart
+++ lib/models/Account.dart
@@ -4,3 +4,9 @@ class Account {
   String name;
   String email;
 }
+
+List<String> getFirstNames(List<Account> accounts) {
+  List<String> firstNames = [];
+  accounts.forEach((element) => firstNames.add(element.name.contains(" ") ? element.name.split(" ")[0] : element.name));
+  return firstNames;
+}
